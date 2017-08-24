b8ef93e63002be8f09fc4abae692d40b064dd9ba
nategarvison <ngarvison@gmail.com>
2017-08-24 15:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  [...kittens, "Charlie"];
+  [...kittens, name];
   kittens;
 }
