6cb2065f041f8ebf604cc96368c541ad9c8adf04
nategarvison <ngarvison@gmail.com>
2017-08-24 15:46:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,3 +36,7 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens];
 }
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length - 1);
+}
